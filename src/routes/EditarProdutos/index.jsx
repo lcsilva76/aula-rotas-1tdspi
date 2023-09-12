5aac2b40d85fce61a0dda8c6be104295982ecd17
lcsilva76 <lsilva@fiap.com.br>
2023-09-12 18:58:52 -0300

    Primeiro Commit

---
+++ src/routes/EditarProdutos/index.jsx
@@ -0,0 +1,25 @@
+import {useParams, useNavigate} from 'react-router-dom'
+import { listaProdutos } from '../../components/listaProdutos'
+
+export default function EditarProdutos(){
+
+    const lista = listaProdutos
+    const navegacao = useNavigate()
+    const {id} = useParams()
+
+    const proc = lista.filter(prod => prod.id == id)
+    const produto = proc[0]
+
+    const salvar = ()=>{
+        alert(`Produto: ${produto.nome} editado com sucesso!`)
+        return navegacao('/produtos')
+    }
+
+    return(
+        <main>
+            <h1>Editar Produtos</h1>
+            <p>Editando os dados do produto: {produto.nome}</p>
+            <button onClick={salvar}>Salvar</button>
+        </main>
+    )
+}
---
+++ src/routes/Produtos/index.jsx
@@ -0,0 +1,20 @@
+import {Link} from 'react-router-dom'
+import { listaProdutos } from '../../components/listaProdutos'
+
+export default function Produtos(){
+
+    return(
+        <main>
+            <h1>Produtos</h1>
+            {
+                listaProdutos.map(prod => (
+                    <div key={prod.id}>
+                        <Link to={`/produtos/editar/${prod.id}`}>
+                            Editar produto: {prod.nome}
+                        </Link>
+                    </div>
+                ))
+            }
+        </main>
+    )
+}
